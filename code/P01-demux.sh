cc2e1358690009cda8711a43023aff159876a762
Carli Jones <64499193+carli-jones@users.noreply.github.com>
2021-01-29 12:43:45 -0500

    Update P01-demux.sh

--- code/P01-demux.sh
+++ code/P01-demux.sh
@@ -2,8 +2,8 @@
 #SBATCH -c 5
 #SBATCH -t 10:00:00
 #SBATCH --mail-type=all
-#SBATCH --mail-user=lpeiffe1@jhu.edu
-#SBATCH --job-name=demux atcc-pilot-v2
+#SBATCH --mail-user=cjone228@jhu.edu
+#SBATCH --job-name=demux all atcc
 
 source ~/.bashrc
 #source activate qiime2-2020.6
@@ -33,12 +33,12 @@ do
 
 qiime tools import \
   --type 'SampleData[SequencesWithQuality]' \
-  --input-path  ../data/manifests/chip$i/chip$i-$REGION-manifest.tsv \
-  --output-path $outDir/$REGION/chip$i-$REGION-demux.qza \
+  --input-path  ../data/manifests/chip$i/atcc-chip$i-$REGION.tsv \
+  --output-path $outDir/$REGION/atcc-chip$i-$REGION-demux.qza \
   --input-format SingleEndFastqManifestPhred33V2
 
 qiime demux summarize \
- --i-data $outDir/$REGION/chip$i-$REGION-demux.qza \
- --o-visualization $outDir/$REGION/chip$i-$REGION-demux.qzv
+ --i-data $outDir/$REGION/atcc-chip$i-$REGION-demux.qza \
+ --o-visualization $outDir/$REGION/atcc-chip$i-$REGION-demux.qzv
 
 done
