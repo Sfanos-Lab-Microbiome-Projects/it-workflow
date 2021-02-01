119269b6b1b1e391c64fe671189d1518cf4eb7ff
Carli Jones <cjone228@jhmi.edu>
2021-02-01 14:26:52 -0500

    added P11-demux code to import patient samples

---
+++ code/P11-demux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -c 12
+#SBATCH -t 10:00:00
+#SBATCH --mail-type=all
+#SBATCH --mail-user=cjone228@jh.edu
+#SBATCH --job-name="demux patient samples"
+#SBATCH --mem=20G
+#SBATCH -p parallel
+#SBATCH --array=1-6
+
+source ~/.bashrc
+#source activate qiime2-2020.6
+
+locale
+LC_ALL=en_US.UTF-8
+export LC_ALL
+LANG="en_US.UTF-8"
+export LANG
+locale
+
+conda activate qiime2-2020.6
+
+outDir=../external/P11-demux
+mkdir -p $outDir
+
+# array for different regions ${SLURM_ARRAY_TASK_ID} goes from 1 to 6
+declare -a arr=("v2" "v3" "v4" "v67" "v8" "v9")
+#TASKID=1
+REGION=${arr[${SLURM_ARRAY_TASK_ID}-1]}
+echo "$REGION"
+mkdir -p $outDir/$REGION
+
+qiime tools import \
+  --type 'SampleData[SequencesWithQuality]' \
+  --input-path  ../data/manifests/patient-chip1-$REGION.tsv \
+  --output-path $outDir/$REGION/patient-chip1-$REGION-demux.qza \
+  --input-format SingleEndFastqManifestPhred33V2
+
+qiime demux summarize \
+ --i-data $outDir/$REGION/patient-chip1-$REGION-demux.qza \
+ --o-visualization $outDir/$REGION/patient-chip1-$REGION-demux.qzv
